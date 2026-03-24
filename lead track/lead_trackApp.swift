fd5f9165f6b37ed047029e2b6642f6657b48a9cd
Benni Rogge <benni.rogge@gmail.com>
2026-03-23 19:58:31 -0700

    Initial Commit

---
+++ lead track/lead_trackApp.swift
@@ -0,0 +1,32 @@
+//
+//  lead_trackApp.swift
+//  lead track
+//
+//  Created by Rogge, Benni on 3/23/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct lead_trackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
